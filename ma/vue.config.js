b0097606b566936faa58561432d673b268d597a1
Maria <maria.co@epitech.eu>
2022-07-10 22:38:16 +0200

    ajout du publicpath

--- ma/vue.config.js
+++ ma/vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
+module.exports = {
+ publicPath: publicPath,
+}
